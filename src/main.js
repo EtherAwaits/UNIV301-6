105925a08df15e19b0511047cd457e0919943b8e
EtherAwaits <brettwilliams343@gmail.com>
2025-11-08 14:17:53 -0600

    Update UI labels
    Changed navbar tab-data from 'alerts' to 'community' in index.html. Updated badge style for 'Career' tag, changed 'Seasonal' to 'Season' in leaderboard menu, and revised leaderboard header to 'Fall Seasonal Leaderboard' with an icon in main.js.

--- src/main.js
+++ src/main.js
@@ -435,7 +435,7 @@ document.addEventListener("DOMContentLoaded", () => {
                             <span class="indicator-item badge badge-secondary mx-10 hover:cursor-pointer clickevent">3 New Posts</span>
                             <article class="article-card clickevent w-full">
                                 <div class="tags">
-                                    <span class="tag badge-secondary">Career</span>
+                                    <span class="tag badge-error">Career</span>
                                 </div>
 
                                 <h3 class="header">Government & Social Services Community</h3>
@@ -519,7 +519,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 display.innerHTML = `
                     <div class="flex items-center gap-4 mb-4">
                     <ul class="menu menu-horizontal clickevent bg-primary card font-bold text-white rounded-box shadow-md">
-                    <li><a class="bg-accent/60">Seasonal</a></li>
+                    <li><a class="bg-accent/60">Season</a></li>
                     <li><a>Yearly</a></li>
                     <li><a>All Time</a></li>
                     </ul>
@@ -541,7 +541,7 @@ document.addEventListener("DOMContentLoaded", () => {
                     </label>
                     </div>
 
-                    <h1 class="header">Fall Season Leaderboard</h1>
+                    <h1 class="header flex gap-1"><svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M6.74534 4H17.3132C17.3132 4 16.4326 17.2571 12.0293 17.2571C9.87826 17.2571 8.56786 14.0935 7.79011 10.8571C6.97574 7.46844 6.74534 4 6.74534 4Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M17.3132 4C17.3132 4 18.2344 3.01733 19 2.99999C20.5 2.96603 20.7773 4 20.7773 4C21.0709 4.60953 21.3057 6.19429 19.8967 7.65715C18.4876 9.12 16.9103 10.4 16.2684 10.8571" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M6.74527 4.00001C6.74527 4.00001 5.78547 3.00614 4.99995 3.00001C3.49995 2.9883 3.22264 4.00001 3.22264 4.00001C2.92908 4.60953 2.69424 6.19429 4.1033 7.65715C5.51235 9.12001 7.14823 10.4 7.79004 10.8572" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path><path d="M8.50662 20C8.50662 18.1714 12.0292 17.2571 12.0292 17.2571C12.0292 17.2571 15.5519 18.1714 15.5519 20H8.50662Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>Fall Seasonal Leaderboard</h1>
 
                     <div class="overflow-x-auto">
                         <table class="table table-zebra">
